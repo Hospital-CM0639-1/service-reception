bbd6b3de1f9ed144743ca5732b27a97b2e672f98
xhrmo1 <hrmolukas@gmail.com>
2024-12-11 09:05:41 +0100

    feat: implementing PatientService and BaseService

---
+++ Infrastructure/Services/BaseService.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Data;
+
+namespace Infrastructure.Services;
+
+public class BaseService(HospitalContext dBContext) : IBaseService
+{
+    public async Task<bool> SaveAsync(bool save)
+    {
+        if (!save) return true;
+        try
+        {
+            await dBContext.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+}
