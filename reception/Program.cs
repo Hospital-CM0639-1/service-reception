8c9715f3e9d54bfbd0b7d2c2d0d26ea5b677570b
xhrmo1 <hrmolukas@gmail.com>
2025-01-07 18:02:28 +0100

    feat: add jwt token

--- reception/Program.cs
+++ reception/Program.cs
@@ -1,6 +1,13 @@
+using System.Security.Cryptography;
 using DataAccessLayer.Data;
 using Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using reception.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,9 +22,24 @@ builder.Services.AddDbContextFactory<HospitalContext>(options =>
         //.UseLazyLoadingProxies()
         .LogTo(s => System.Diagnostics.Debug.WriteLine(s))
     );
-var connectionString = builder.Configuration.GetConnectionString("Psql");
 
 builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
+    {
+        var rsaKey = RSA.Create();
+        rsaKey.ImportFromPem(File.ReadAllText(builder.Configuration.GetSection("Jwt:PublicKeyPath").Value));
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateActor = false,
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = false,   
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new RsaSecurityKey(rsaKey)
+        }; 
+    });
+builder.Services.AddAuthorization();
+
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<IPatientService, PatientService>();
 builder.Services.AddScoped<IDoctorService, DoctorService>();
@@ -28,7 +50,7 @@ builder.Services.AddScoped<IManagerService, ManagerService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle    
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,6 +61,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
---
+++ reception/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace reception.Swagger;
+
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    public void Configure(SwaggerGenOptions options)
+    {
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                ArraySegment<string>.Empty
+            }
+        });
+    }
+}
